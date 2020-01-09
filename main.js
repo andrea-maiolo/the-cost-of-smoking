966b7f6ffd6046c8323fce0eb8d962a4879e7bda
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-01-09 19:58:25 +0800

    I added more tools to the html file so now it is easier to calulate the result, still working on the js file to get the current year

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-let currentYear = 2020;
-let years = document.querySelector('.start-year');
-let result = document.querySelector("res");
+let currentYear = new Date;
+
+let startYear = document.getElementsByClassName('.startYear');
+
+let y = currentYear.getFullYear() - startYear;
+
+let numberOfPacks = document.getElementsByClassName('.packsOfCigarettes');
+
+let money = document.getElementsByClassName("currency");
+
+let result = document.onclick()
+
+//document.write(new Date().getFullYear()) this is the shortest way to display the current year on a website modifyng a DOM
