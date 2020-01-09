4f110de24fbea5b54e86774693519ee791c61f79
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-01-10 00:10:27 +0800

    I changed the main.js file and added the function onclick, minor changes to html file also

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ let currentYear = new Date().getFullYear();
 
 let startYear = document.getElementsByClassName('.startYear');
 
-let y = currentYear.getFullYear() - startYear;
+
 
 let numberOfPacks = document.getElementsByClassName('.packsOfCigarettes');
 
 let money = document.getElementsByClassName("currency");
 
-let result = document.onclicck; 
+document.getElementsByClassName('result').onclick = function getResult (//quali sono gli argomenti di questa funzione){
+    //let y = currentYear.getFullYear() - startYear; y * (numbersOfPacks * day/week/month/year) * avaragePackCost  
+}
