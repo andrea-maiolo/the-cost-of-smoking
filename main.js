c1fc0997ad9e264c2175fd48817ea74cdf692553
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-01-10 18:55:52 +0800

    added on click function

--- main.js
+++ main.js
@@ -9,5 +9,11 @@ let numberOfPacks = document.getElementsByClassName('.packsOfCigarettes');
 let money = document.getElementsByClassName("currency");
 
 document.getElementsByClassName('result').onclick = function getResult (//quali sono gli argomenti di questa funzione){
-    //let y = currentYear.getFullYear() - startYear; y * (numbersOfPacks * day/week/month/year) * avaragePackCost  
-}
+    //let y = currentYear.getFullYear() - startYear; y * (numbersOfPacks * day/week/month/year) * avaragePackCost  }
+
+//this will create a alert window on click of the button
+    //const myButton = document.querySelector('button');
+
+//myButton.onclick = function() {
+   // alert('hello');
+ // }
