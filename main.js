ebb5b5c9e546f77f495a8b96353f154beb6c23f1
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-04-19 12:58:43 +0200

    smoking calculator

--- main.js
+++ main.js
@@ -1,19 +1,51 @@
-let currentYear = new Date().getFullYear();
+var min = 1,
+	max = 100,
+	packs = document.getElementById('costPack');
+    cigarettes = document.getElementById('cigarettes');
+    cigarettesSmoked = document.getElementById('ciggySmoked');
+    smokingYears = document.getElementById('smokingTime');
+    result = document.querySelector('#result');
+    calculation = document.querySelector('#calculation');
+    calculation.addEventListener('click', calculate);
+    currency = document.querySelector('#currency');
+    
 
-let startYear = document.getElementsByClassName('.startYear');
+for (var i = min; i <= max; i++) {
+	var opt = document.createElement('option');
+	opt.value = i;
+	opt.innerHTML = i;
+	costPack.appendChild(opt);
+}
+for (var i = min; i <= max; i++) {
+	var opt = document.createElement('option');
+	opt.value = i;
+	opt.innerHTML = i;
+	cigarettes.appendChild(opt);
+}
+for (var i = min; i <= max; i++) {
+	var opt = document.createElement('option');
+	opt.value = i;
+	opt.innerHTML = i;
+	ciggySmoked.appendChild(opt);
+}
+for (var i = min; i <= max; i++) {
+	var opt = document.createElement('option');
+	opt.value = i;
+	opt.innerHTML = i;
+	smokingTime.appendChild(opt);
+}
 
 
-
-let numberOfPacks = document.getElementsByClassName('.packsOfCigarettes');
-
-let money = document.getElementsByClassName("currency");
-
-document.getElementsByClassName('result').onclick = function getResult (//quali sono gli argomenti di questa funzione){
-    //let y = currentYear.getFullYear() - startYear; y * (numbersOfPacks * day/week/month/year) * avaragePackCost  }
-
-//this will create a alert window on click of the button
-    //const myButton = document.querySelector('button');
-
-//myButton.onclick = function() {
-   // alert('hello');
- // }
+function calculate() {
+	const frequency = document.querySelector('#frequency');
+	const smokingTime = document.querySelector('#smokingTime');
+	const howLong = document.querySelector('#howLong');
+	let timeSmoking = (howLong.value * smokingTime.value);
+	let x = (ciggySmoked.value * timeSmoking) / frequency.value;
+	let a = x / cigarettes.value;
+	let total = a * costPack.value;
+    const para = document.createElement('p');
+    para.setAttribute('id','totalAmount');
+	para.textContent = 'You have spent '+total + ' ' + currency.value +' till now';
+    result.appendChild(para);
+}
